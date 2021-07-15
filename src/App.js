f0593f3ce290315ecb93446ab7893a958fdfdf34
Joewings-jw <ngugijoseph830@gmail.com>
2021-07-15 14:59:47 +0300

    route to cart screen

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ProductView from './views/productView';
 
 import './App.css';
 import HomeView from './views/homeView';
+import CartView from './views/cartView';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <main className='container py-6'>
         <Route path='/product/:id' component={ ProductView }/>
+        <Route path='/cart/:id?' component={ CartView }/>
         <Route path='/' component={ HomeView } exact/>
       </main>
       <Footer />
