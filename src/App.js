d21be16987b910d03a7bc836df5b892ba46a0d4d
Joewings-jw <ngugijoseph830@gmail.com>
2021-07-15 16:49:10 +0300

    route to login

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import ProductView from './views/productView';
 import './App.css';
 import HomeView from './views/homeView';
 import CartView from './views/cartView';
+import Login from './views/login';
 
 function App() {
   return (
     <Router>
       <Header />
       <main className='container py-6'>
+        <Route path='/login' component={Login}/>
         <Route path='/product/:id' component={ ProductView }/>
         <Route path='/cart/:id?' component={ CartView }/>
         <Route path='/' component={ HomeView } exact/>
