252ceabf236ef4d88c3ab619e73954a7e49d6425
Joewings-jw <ngugijoseph830@gmail.com>
2021-07-15 15:03:28 +0300

    initialized state for the cart

--- src/state/store/store.js
+++ src/state/store/store.js
@@ -1,17 +1,24 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import {productListReducer, productDetailsReducer} from '../reducers/productReducers'
+import { cartReducer } from '../reducers/cartReducers'
 
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer,
 })
 
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? 
+    JSON.parse(localStorage.getItem('cartItems')) : []
 
 
+const initialState = {
+    cart: { cartItems: cartItemsFromStorage}
+}
 const middleware = [thunk]
 
-const store = createStore(reducer,applyMiddleware(...middleware))
+const store = createStore(reducer,initialState, applyMiddleware(...middleware))
 
 export default store
