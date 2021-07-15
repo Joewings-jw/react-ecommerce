9055cbcec5e6e228f28ea760062e3d2aded42fb9
Joewings-jw <ngugijoseph830@gmail.com>
2021-07-15 12:47:16 +0300

    created screen view for home and product

---
+++ src/views/homeView.js
@@ -0,0 +1,40 @@
+import {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {listProducts} from '../state/actions/productActions'
+import { Loader, Message, Product} from '../components'
+
+const HomeView = () => {
+    const dispatch = useDispatch()
+    const {loading, error, products} = useSelector(state => state.productList)
+    
+
+    useEffect(() => {
+        dispatch(listProducts())
+    }, [dispatch]) 
+
+    return (
+        <>
+            <h1 className='mb-5 text-2xl font-bold'>Latest products</h1>
+            {
+                loading ? (
+                    <Loader>lo...ading</Loader>
+                ): error ? (
+                    <Message>{error}</Message>
+                ):(
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                    {products.map(product => 
+                        <div key={product._id}>
+                            <Product product={product}/>
+                        </div>
+                    )}
+                </div>
+                ) 
+            }
+        </>
+    );
+
+
+
+}
+
+export default HomeView;
---
+++ src/views/productView.js
@@ -0,0 +1,79 @@
+import React, {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import {ArrowLeftCircle, ShoppingBag } from 'react-feather'
+import {listProductDetails} from '../state/actions/productActions'
+import { Loader, Message} from '../components'
+
+
+const ProductView = ({history, match})=>{
+    const [quantity, setQuantity] = useState(1)
+    const dispatch = useDispatch()
+    const { loading, error, product } = useSelector(state => state.productDetails)
+
+
+    useEffect(() => {
+        dispatch(listProductDetails(match.params.id))
+    }, [dispatch, match]) 
+
+    const addToCartHandler = () => {
+        history.push(`/cart/${match.params.id}?quantity=${quantity}`)
+    }
+
+    return (
+        <>
+            <div className='mb-5'>
+                <Link title='Go Back' to='/' className='py-1 btn'>
+                    <ArrowLeftCircle className='mr-0 align-bottom feather-icon'/>
+                </Link>
+            </div>
+            {
+                loading ? (
+                    <Loader />
+                ): error ? (
+                    <Message>{error}</Message>
+                ): (
+                    <div className='flex flex-col md:flex-row'>
+
+                        <div className=' md:w-3/5'>
+                            <img className='' src={product.image} alt={product.name}/>
+                        </div>
+                        <div className='md:w-1/5'>
+                            <h3 className='text-4xl leading-none'>{product.name}</h3>
+                            <h3 className='text-lg'><span className='font-bold'>${product.price}</span></h3>
+                            <p>{product.description}</p>
+                        </div>
+                        <div className='md:w-1/5'>
+                            <h3 className='text-lg'>Price: <span className='font-bold'>${product.price}</span></h3>
+                            <h3>Status: {product.countInStock > 0 ? 'In stock' : 'Out of stock'}</h3>
+                            {product.countInStock > 0 && (
+                                <div className='my-3'>
+                                    Quantity:
+                                    <select name="quantity" id="quantity" value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+                                        {[...Array(product.countInStock).keys()].map(x => (
+                                            <option key={x+1} value ={x+1}>{x+1}</option>
+                                        ))}
+                                    </select>
+                                </div>
+                            )}
+                            <div>
+                                <button
+                                onClick={addToCartHandler}
+                                className={product.countInStock === 0 ? 'flex items-center justify-center w-full cursor-not-allowed btn' : 'flex items-center justify-center w-full btn'}
+                                >
+                                    <ShoppingBag className='w-5 h-5 mr-3'/>
+                                    Add to cart
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+        </>
+    );
+    
+}
+
+
+
+export default ProductView
