272ec9dda84ca95615ca620d154f1c2bb5976129
Joewings-jw <ngugijoseph830@gmail.com>
2021-07-15 12:45:08 +0300

    added app routes views/home views/products

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { Header, Footer } from './components'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import ProductView from './views/productView';
+
+
 import './App.css';
+import HomeView from './views/homeView';
 
 function App() {
   return (
-    <div className="App" class="p-6 items-center justify-center">
-
-      <h3 className="text-center text-indigo-900 uppercase">
-       React ecommerce site
-      </h3>
-    </div>
+    <Router>
+      <Header />
+      <main className='container py-6'>
+        <Route path='/product/:id' component={ ProductView }/>
+        <Route path='/' component={ HomeView } exact/>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
